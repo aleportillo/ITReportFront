bb29dd819459229a0b91ef5a649f37515b892a7a
Freida Portillo <alejandrabypoca@gmail.com>
2022-09-27 22:49:58 -0600

    💄 feat: add section header

--- src/app/user/section/section.component.ts
+++ src/app/user/section/section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenSize } from 'src/app/core/models/tools/screen-size.model';
+import { HelpersService } from 'src/app/core/services/internal/helpers.service';
 
 @Component( {
 	selector    : 'app-section',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 } )
 export class SectionComponent implements OnInit {
 
-	constructor() { }
+	type = 'room';
+	idSection = '478ASD';
+	
+	screenSize: ScreenSize = new ScreenSize();
+
+	constructor(
+		private _helpersService : HelpersService
+	) { }
 
 	ngOnInit(): void {
+		this.screenService();
+	}
+
+	// -------------------------------------------------- ANCHOR: SUBS
+
+	screenService(){
+		this._helpersService.screenSize$.subscribe( ( response ) => {
+			this.screenSize = response;
+		} );
 	}
 
 }
