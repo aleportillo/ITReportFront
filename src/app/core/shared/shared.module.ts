084110e9aba8a841b506e63426f85afce92dc5ce
Freida Portillo <alejandrabypoca@gmail.com>
2022-10-01 18:19:52 -0600

    ➕ add simplebar (custom scroll)

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -4,11 +4,12 @@ import inputs from './inputs';
 import components from './components';
 import { MaterialModule } from 'src/app/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SimplebarAngularModule } from 'simplebar-angular';
 
 @NgModule( {
 	declarations : [ ...inputs, ...components ],
 	imports      : [ CommonModule, MaterialModule, FormsModule, ReactiveFormsModule ],
-	exports      : [ ...inputs, ...components ]
+	exports      : [ ...inputs, ...components, SimplebarAngularModule ]
 } )
 
 export class SharedModule { }
