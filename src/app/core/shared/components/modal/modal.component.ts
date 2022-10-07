7ec6de8d499cf5f6a9c53d36d8279424d5ed480c
Freida Portillo <alejandrabypoca@gmail.com>
2022-10-06 21:16:58 -0600

    WIP: modal
    - load options in the incident select according to what you put in the category select

--- src/app/core/shared/components/modal/modal.component.ts
+++ src/app/core/shared/components/modal/modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IFormInput } from 'src/app/core/models/tools/form-input.modal';
 import { IModalData } from 'src/app/core/models/tools/modal-data';
+import { FormService } from 'src/app/core/services/internal/form.service';
 
 @Component( {
 	selector    : 'app-modal',
@@ -10,17 +13,30 @@ import { IModalData } from 'src/app/core/models/tools/modal-data';
 export class ModalComponent implements OnInit {
 
 	modalData: IModalData;
+	form : FormGroup  = new FormGroup( {} );
+	editData : any = null;
 
 	constructor(
+		private _formService : FormService,
 		private _dialogRef: MatDialogRef<ModalComponent>,
         @Inject( MAT_DIALOG_DATA ) data : IModalData
 	) {
 		this.modalData = data;
-		console.log(data);
-		
+		// console.log( data );
 	}
 
 	ngOnInit(): void {
+		this.form = this._formService.createForm( this.modalData.form );
+	}
+
+	primaryAction(){
+		// console.log(this.modalData.typeModal);
+		
+		// console.log( this.form.value );
+		this._formService.formData$.next( {
+			newData  : { ...this.form.value },
+			editData : ( this.editData ) ? { ...this.editData } : null 
+		} );		
 	}
 	
 	closeModal(){
@@ -29,5 +45,14 @@ export class ModalComponent implements OnInit {
 		// data => console.log("Dialog output:", data)
 		// );
 		this._dialogRef.close();
+	}
+
+	changeOption( input: IFormInput, event: any ){
+		if ( input.name === 'categoria' ){
+			const incidenteField = this.modalData.form.find( ( field : IFormInput ) => field.name === 'incidente' );
+			if ( !incidenteField ) { return; }
+			incidenteField.options =  incidenteField?.allOptions[event];
+			this.form.get( 'incidente' )?.reset();
+		}
 	}
 }
--- src/app/core/shared/inputs/input-select/input-select.component.ts
+++ src/app/core/shared/inputs/input-select/input-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { IFormInput, FormInput } from 'src/app/core/models/tools/form-input.modal';
 
@@ -11,14 +11,18 @@ export class InputSelectComponent implements OnInit {
 
 	@Input() formInput : IFormInput = new FormInput();
 	@Input() form : FormGroup = new FormGroup( {} );
+	@Output() changeOptionEmmiter = new EventEmitter<any>();
 
 	constructor() { }
 
 	ngOnInit(): void {
-		console.log(this.formInput);
-		console.log(this.form.value);
-		
-		
+		// console.log(this.formInput);
+		// console.log(this.form.value);
+	}
+
+	changeOption( event: any ){
+		// console.log(event);
+		this.changeOptionEmmiter.emit( event.value );
 	}
 
 }
