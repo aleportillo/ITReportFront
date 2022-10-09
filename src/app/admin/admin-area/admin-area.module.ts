0a52a3410347331dd614ecbe76a2febc75644942
Freida Portillo <alejandrabypoca@gmail.com>
2022-10-08 22:26:28 -0600

    🚧 WIP: admin area
    - add header

--- src/app/admin/admin-area/admin-area.module.ts
+++ src/app/admin/admin-area/admin-area.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminAreaRoutingModule } from './admin-area-routing.module';
-
-
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/core/shared/shared.module';
+import { SectionRoutingModule } from 'src/app/user/section/section-routing.module';
+import { AdminAreaComponent } from './admin-area.component';
+import components from '../components';
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule( {
-	declarations : [],
+	declarations : [ AdminAreaComponent, ...components ],
 	imports      : [
 		CommonModule,
-		AdminAreaRoutingModule
+		AdminAreaRoutingModule,
+		SectionRoutingModule,
+		RouterModule,
+		SharedModule,
+		MatMenuModule
 	]
 } )
 export class AdminAreaModule { }
---
+++ src/app/admin/components/index.ts
@@ -0,0 +1,5 @@
+import { HeaderComponent } from './header/header.component';
+
+const components = [HeaderComponent];
+
+export default components;
