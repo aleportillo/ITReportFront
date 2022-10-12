8cdfff2a2bdbb0bc6b555d1959920748c1a56297
Freida Portillo <alejandrabypoca@gmail.com>
2022-10-11 21:34:57 -0600

    💄style: update style files and add edit icon

--- src/app/core/shared/components/list-row/list-row.component.ts
+++ src/app/core/shared/components/list-row/list-row.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IActiveReport } from 'src/app/core/models/tools/reports/active-report.modal';
 
-@Component({
-  selector: 'app-list-row',
-  templateUrl: './list-row.component.html',
-  styleUrls: ['./list-row.component.css']
-})
+@Component( {
+	selector    : 'app-list-row',
+	templateUrl : './list-row.component.html',
+	styleUrls   : ['./list-row.component.css']
+} )
 export class ListRowComponent implements OnInit {
 
-  constructor() { }
+	@Input() report : IActiveReport | any;
+	@Input() rowTemplate : any;
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
